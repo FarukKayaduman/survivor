4e43d43157f73a48863c267822f3ed1d800da91f
Faruk <farukkayaduman@outlook.com>
2023-07-11 15:02:54 +0300

    feat: Add ScriptableObject GameEvents and OnDeath OnHit SO assets.

---
+++ Assets/Scripts/ScriptableObjects/GameEvent/GameEvent.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObjects.GameEvent
+{
+    [CreateAssetMenu(fileName = "New Game Event", menuName = "Game Event")]
+    public class GameEvent : ScriptableObject
+    {
+        private readonly List<GameEventListener> _listeners = new List<GameEventListener>();
+
+        public void Raise()
+        {
+            for (int i = _listeners.Count - 1; i >= 0; i--)
+            {
+                _listeners[i].OnEventRaised();
+            }
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            _listeners.Remove(listener);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/GameEvent/GameEventListener.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ScriptableObjects.GameEvent
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [SerializeField] private GameEvent gameEvent;
+        [SerializeField] private UnityEvent response;
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+    }
+}
